1e4230762d007fc964fda5c1ef6f9c57e1ccd40c
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-03-07 13:15:18 +0100

    feat: implemented Create Prompt

---
+++ app/api/prompt/new/route.ts
@@ -0,0 +1,21 @@
+import {connectToDB} from "@/utils/database";
+import Prompt from "@/models/prompt";;
+
+export const POST = async (req) => {
+    const data = await req.json()
+    const { userId, prompt, tag } = data
+
+    try{
+        await connectToDB();
+        const newPrompt = await Prompt.create({
+            creator: userId,
+            prompt,
+            tag
+        })
+        debugger
+        return new Response(JSON.stringify(newPrompt), {status: 201})
+    } catch (error) {
+        console.error(error)
+        return new Response("Failed to create a anew prompt", { status: 500 })
+    }
+}
