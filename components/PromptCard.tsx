a8c961374cd5c9150d878a5d5c62a10ae1ae3c55
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-03-07 15:59:40 +0100

    feat: everything

--- components/PromptCard.tsx
+++ components/PromptCard.tsx
@@ -1,10 +1,15 @@
 "use client"
 import Image from "next/image";
 import {useState} from "react";
+import {useSession} from "next-auth/react";
+import {usePathname, useRouter} from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     // console.log(post.creator)
     const [copied, setCopied] = useState('');
+    const {data: session} = useSession()
+    const pathName = usePathname();
+    const router = useRouter()
 
     const handleCopy = () => {
         setCopied(post.prompt);
@@ -49,9 +54,20 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                 className='font-inter text-sm blue_gradient cursor-pointer'
                 onClick={() => handleTagClick && handleTagClick(post.tag)}
             >
-                {post.tag}
+                #{post.tag}
             </p>
-
+            {session?.user.id === post.creator._id &&
+            pathName === '/profile' &&
+                (
+                    <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+                        <p className='font-inter text-sm green_gradient cursor-pointer' onClick={handleEdit}>
+                            Edit
+                        </p>
+                        <p className='font-inter text-sm orange_gradient cursor-pointer' onClick={handleDelete}>
+                            Delete
+                        </p>
+                    </div>
+                )}
         </div>
     );
 };
